1b1d58e8eaf72937078aed295e7eba34f5657ba7
Shafira Naya <shfrnaya@gmail.com>
2021-11-24 14:53:10 +0700

    pengelolaan request done

--- controller/request.js
+++ controller/request.js
@@ -5,6 +5,7 @@ const Request = require("../models/Request");
 const BahanBaku = require("../models/BahanBaku");
 const Resep = require("../models/Resep");
 const Dorayaki = require("../models/Dorayaki");
+const LogRequest = require("../models/LogRequest");
 const { Sequelize } = require("sequelize");
 
 const Op = Sequelize.Op;
@@ -29,137 +30,74 @@ router.route("/").get(async (req, res) => {
 // pabrik). Pabrik hanya perlu mengupdate status request dan toko akan secara
 // berkala memantau status request yang pernah diajukannya.
 
-router.route("/:action/:id").get(async (req, res) => {
+router.route("/:action/:id").put(async (req, res) => {
   // note: id disini itu id request kayaknya?
-  const { action, id } = req.params;
+  const { id, action } = req.params;
+  // const { action } = req.body;
   // const { idDorayaki, jumlah} = req.body;
-
-
-  if (action == "accept") {
-    // dari id request, cari id dorayaki
-    const selectedRequest = await Request.findOne({where: {id}});
-    if (selectedRequest) {
-      // res.status(200).json({ success: true, ...selectedRequest.dataValues})
-      // cari id dorayaki
-      const id_dorayaki = selectedRequest.id_dorayaki;
-      // const id_dorayaki = (await selectedRequest)._attributes['id_dorayaki'];
-      // const id_dorayaki = selectedRequest({attributes: ['id_dorayaki']});
-      console.log("ID DORAYAKI: ",id_dorayaki);
-
-      // ini agak gaperlu?
-      // const selectedDorayaki = await Dorayaki.findOne({where: {id}});
-      // console.log("dorayaki yg ada di requestnya: ",selectedDorayaki);
-
-      // dari dorayaki, cari bahan baku yg dibutuhkan (lewat resep)
-      const listIdBahanBaku = [];
-      const requiredBahanBakus = await Resep.findAll({where: {id_dorayaki}});
-      console.log("list id bahanbaku: ",requiredBahanBakus);
-      requiredBahanBakus.forEach(element => {
-        listIdBahanBaku.push(element.dataValues['id_bahanbaku']);
-      });
-      console.log("LISTTT id bahanbaku: ", listIdBahanBaku);
-
-
-      // kurangin bahan bakunya (pake looping) -> gmn ya cara update beberapa tuple bahanbaku?
+  
+  // cari id request
+  const allRequests = await Request.findAll();
+  const selectedRequest = await Request.findOne({where: {id}});
+  const actionAvailable = selectedRequest.status == "waiting";
+  
+  // TODO kalo udah accepted / declined, gabisa diapa2in
+  if ((selectedRequest) ) {
+    if (actionAvailable) {
+      if (action == "accept") {
+        // dari id request, cari id dorayaki
+        const id_dorayaki = selectedRequest.id_dorayaki;
       
-      // cari dari list bahan baku (stoknya)
-      const stokBahanBakus = await BahanBaku.findAll(
-      {
-        where: {
-          id: {
-            [Op.in]: listIdBahanBaku
+        // dari dorayaki, cari bahan baku yg dibutuhkan (lewat resep)
+        const listIdBahanBaku = [];
+        const requiredBahanBakus = await Resep.findAll({where: {id_dorayaki}});
+        // console.log("list id bahanbaku: ",requiredBahanBakus);
+        requiredBahanBakus.forEach(element => {
+          listIdBahanBaku.push(element.dataValues['id_bahanbaku']);
+        });
+        // console.log("LISTTT id bahanbaku: ", listIdBahanBaku);
+  
+        // cari dari list bahan baku (stoknya)
+        const stokBahanBakus = await BahanBaku.findAll(
+        {
+          where: {
+            id: {
+              [Op.in]: listIdBahanBaku
+            }
           }
+        });
+  
+        // updateBahanBaku nya TAPI HARUS DICEK DULU CUKUP APA ENGGA !!!! KALO ENGGA TERPAKSA GABISA ???
+        // gausah ada pengecekan deh
+  
+        let updateValue = new Array(stokBahanBakus.length);
+        for (let i = 0; i < stokBahanBakus.length; i++) {
+          stokBahanBakus[i].dataValues["stok"] -= selectedRequest.dataValues["jumlah"] * requiredBahanBakus[i].dataValues["jumlah"];
+          updateValue[i] = {};
+          updateValue[i]["id"] = stokBahanBakus[i].dataValues["id"];
+          updateValue[i]["stok"] = stokBahanBakus[i].dataValues["stok"];
+          BahanBaku.update(updateValue[i], { where: {id: listIdBahanBaku[i]}});
         }
-      });
+        // update status jadi accepted 
+        selectedRequest.update({status:"accepted"});
+        // nambahin ke log request ??? (ga kayanya)
+        res.status(200).json({ success: true, allRequests});
 
-    
-      // updateBahanBaku nya TAPI HARUS DICEK DULU CUKUP APA ENGGA !!!! KALO ENGGA TERPAKSA GABISA ???
-      // gausah ada pengecekan deh
-
-      let updateValue = new Array(stokBahanBakus.length);
-      for (let i = 0; i < stokBahanBakus.length; i++) {
-        stokBahanBakus[i].dataValues["stok"] -= selectedRequest.dataValues["jumlah"] * requiredBahanBakus[i].dataValues["jumlah"];
-        updateValue[i] = {};
-        updateValue[i]["id"] = stokBahanBakus[i].dataValues["id"];
-        updateValue[i]["stok"] = stokBahanBakus[i].dataValues["stok"];
-        BahanBaku.update(updateValue[i], { where: {id: listIdBahanBaku[i]}});
-        //   .then(async (result) => {
-        //     res
-        //       .status(200)
-        //       .json({ success: true, stokBahanBakus});
-        // })
-        // .catch((err) => {
-        //   res.status(500).json({ succes: false, msg: "Internal Server Error" });
-        // });
- 
       }
-      // bingung res nya gitu karena updatenya kyk satu2, gabisa update sekaligus gt bingung,, 
-      res.status(200).json({ success: true, stokBahanBakus });
-      // const updateValueObj = {};
-      // let updateValueObj = {...updateValue};
-      // updateValue.forEach(obj => {
-      //   updateValueObj[obj["id"]] = obj["id"];
-      //   updateValueObj[obj["stok"]] = obj["stok"];
-
-      // })
-    //   BahanBaku.update(updateValue, 
-    //     {fields: ['stok'],
-    //       where: {
-    //     id: {
-    //       [Op.in]: listIdBahanBaku
-    //     },
-        
-    //   }
-    // }
-    //   )
-    //   .then(async (result) => {
-    //       res
-    //         .status(200)
-    //         .json({ success: true, stokBahanBakus });
-    //   })
-    //   .catch((err) => {
-    //     res.status(500).json({ succes: false, msg: "Internal Server Error" });
-    //   });
-      console.log("updatevalue:",updateValue);
-      // console.log("updatevalue:",updateValueObj);
-
-      console.log("Sesudah:",stokBahanBakus);
-
- 
-      // stokBahanBakus.forEach(element => {
-      //   element.dataValues["stok"] -= (selectedRequest["jumlah"] * requiredBahanBakus["jumlah"]);
-      // });
-
-      // updateValue["stok"] = 
-      // BahanBaku.update()
-      // res.status(200).json({success: true, selRequest: selectedRequest.dataValues, idDorayaki: id_dorayaki, reqBahanBaku: requiredBahanBakus,stokBahanBaku: stokBahanBakus})
+      else if (action == "decline") {
+        // TODO kalo decline gimana
+        // tinggal update status jadi declined?
+        selectedRequest.update({status:"declined"});
+        res.status(200).json({ success: true, allRequests});
+      }
     }
-    // else {
-    //   res.status(404).json({success: false, msg: "Request not available"})
-    // }
-
-  if (action == "decline") {
-    // TODO kalo decline gimana
-
-    // tinggal update status jadi declined
+    else {
+      res.status(404).json({success: false, allRequests, msg: "Action not available, request has already accepted or declined"})
+    } 
   }
-
-
-    // steps
-    
-    // dari id_dorayaki, cari bahanbakunya (lewat resep)
- 
-    // jadi cari resep lewat id dorayaki nanti tau butuh bahan baku apa aja
-
-    // cari bahanbaku
-
-    // kurangin bahan baku
-
+  else {
+    res.status(404).json({success: false, allRequests, msg: "Request not available"})
   }
-
-
-
-
 });
 
 module.exports = router;
--- routes.js
+++ routes.js
@@ -7,12 +7,12 @@ const bahanbaku = require("./controller/bahanbaku");
 // const mail = require("./controller/mail");
 const dorayaki = require("./controller/dorayaki");
 
-const requests = require("./controller/request");
+const request = require("./controller/request");
 router.use("/bahanbaku", bahanbaku);
 // router.use("/mail", mail);
 router.use("/dorayaki", dorayaki);
 
-router.use("/requests",requests);
+router.use("/requests",request);
 
 
 module.exports = router;
